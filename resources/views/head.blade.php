676dfeb1143d2be0c4276b64e92a54cdcf19dd43
Shin-Fu <shinfu619@gmail.com>
2020-04-14 18:32:05 +0800

    init laravel project

---
+++ resources/views/head.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Todo list</title>
+    <link rel="stylesheet" href="/css/jquery-ui.min.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css">
+
+    <script src="/js/jquery-3.4.1.min.js"></script>
+    <script src="/js/jquery-ui.min.js"></script>
+    <script src="/js/app.js"></script>
+    <script src="/js/handlebars-v4.7.3.js"></script>
+
+  </head>
+  <body>
