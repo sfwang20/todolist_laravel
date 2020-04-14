2b11bda415001d2d0be1597408615a6f4035c24a
Shin-Fu <shinfu619@gmail.com>
2020-04-14 18:33:27 +0800

    add migrations  create todos table

---
+++ database/migrations/2020_03_21_054004_create_todos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('content');
+            $table->boolean('is_complete');
+            $table->integer('order');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('todos', function(Blueprint $table){
+            $table->dropForeign(['user_id']);
+        });
+
+        Schema::dropIfExists('todos');
+    }
+}
